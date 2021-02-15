f15865924dc1067a1c20344c7b6fd414c1e7613b
oopanpan <oopanpan@gmail.com>
2021-02-15 11:56:49 -0800

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_182908) do
+ActiveRecord::Schema.define(version: 2021_02_15_183612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_02_12_182908) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "bio"
     t.string "favorite_drink"
     t.datetime "created_at", precision: 6, null: false
