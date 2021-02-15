ef72fb7a836cdc46108da11c9d4a95e1494d99ee
oopanpan <oopanpan@gmail.com>
2021-02-15 15:27:33 -0800

    session controller built

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_action :find_user, only: :show
+
     def new
         @user = User.new
     end
@@ -8,13 +10,17 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to user_path
+            redirect_to user_path(@user)
         else
             render :new
         end
     end
 
     private
+
+    def find_user
+        @user = User.find(@user.id)
+    end
     
     def user_params
         params.require(:user).permit(:username, :password, :password_confirmation, :bio, :favorite_drink)
