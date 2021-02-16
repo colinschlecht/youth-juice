b6441f51f8f17504fd4df4022347d41dce505737
oopanpan <oopanpan@gmail.com>
2021-02-15 18:27:50 -0800

    before merge

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,42 @@
 class IngredientsController < ApplicationController
+
+    before_action :find_ingredient, only: [:show, :eidt, :update]
+
+    def index
+        @ingredients = Ingredient.all
+    end
+
+    def show
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def new
+        @ingredient = Ingredient.new
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end 
+
+    def destroy
+
+    end
+
+    private
+
+    def find_ingredient
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name, :description, :ingredient_type)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create'
+  post '/sessions', to: 'sessions#logout', as: 'logout'
 end
