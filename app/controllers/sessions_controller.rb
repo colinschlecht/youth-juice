fc077b7f9a5301b4eb75aa87eebb53ccf5ba49ee
oopanpan <oopanpan@gmail.com>
2021-02-15 20:20:29 -0800

    built ingredient edit view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        if user
-            return head(:forbidden) unless user.authenticate(params[:password])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
@@ -19,11 +18,8 @@ class SessionsController < ApplicationController
         end
     end
 
-<<<<<<< HEAD
-=======
     def logout
         session.delete :user_id
         redirect_to root_path
     end
->>>>>>> origin/colin-wip-postgres
 end
