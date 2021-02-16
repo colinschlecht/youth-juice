15cee1399195742a99ff73d284e9508eccb22587
oopanpan <oopanpan@gmail.com>
2021-02-15 20:19:51 -0800

    built ingredient edit view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -39,4 +39,8 @@ class IngredientsController < ApplicationController
     def ingredient_params
         params.require(:ingredient).permit(:name, :description, :ingredient_type)
     end
+
+    def ingredient_type
+        @ingregident_type = ["alcohol",]
+    end
 end
