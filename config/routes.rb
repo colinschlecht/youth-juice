a385e7a23b2fbddc844e53b8a366cb4413ee196f
oopanpan <oopanpan@gmail.com>
2021-02-15 16:06:21 -0800

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new', as: 'login'
 end
