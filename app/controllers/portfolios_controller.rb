7370073d78d510eb817ca02c2b50b3aabaad61f1
oopanpan <oopanpan@gmail.com>
2021-02-17 19:37:30 -0800

    add to collection wip

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,10 +4,12 @@ class PortfoliosController < ApplicationController
     #     @user = User.find(session[:user_id])
     #     @recipes = @user.recipes
     # end
-
-    def create
-        @recipe = 
-        current_user.recipes << @recipe
+    def index
+        current_user
+        Portfolio.create(user: @current_user, recipe: flash[:recipe])
         redirect_to recipe_path(flash[:recipe])
+    end
+
+    def new_portfolio
     end
 end
